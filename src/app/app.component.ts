e895934a7e89ca8cc9526facf4c59156af1ed160
Safa Abidi <61377020+safa-abidi@users.noreply.github.com>
2021-03-04 23:22:05 +0100

    ui and adding/updating functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'emlpoyeemanager';
+export class AppComponent implements OnInit {
+  public employees: Employee[] = [];
+  public editEmployee!: Employee;
+
+  constructor(private employeeService: EmployeeService) {}
+
+  //have to override ngoninit
+  //gonna load the emlpoyees asap
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-emlpoyee-form')?.click();
+
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+
+    if (mode === 'edit') {
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
 }
---
+++ src/app/employee.service.ts
@@ -0,0 +1,39 @@
+import { environment } from './../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  //comm with backend
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(
+      `${this.apiServerUrl}/employee/add`,
+      employee
+    );
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(
+      `${this.apiServerUrl}/employee/update`,
+      employee
+    );
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServerUrl}/employee/delete/${employeeId}`
+    );
+  }
+}
